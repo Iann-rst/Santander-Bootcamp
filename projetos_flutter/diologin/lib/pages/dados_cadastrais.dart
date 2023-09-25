a61d85da25408c7dd73bcc50654d5f185bbcc6d1
Iann Rodrigues <iannrst11@gmail.com>
2023-09-25 17:36:35 -0300

    feat: Criando tela de dados cadastrais

---
+++ projetos_flutter/diologin/lib/pages/dados_cadastrais.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class InfoPage extends StatelessWidget {
+  final String title;
+  final List<String> dados;
+
+  const InfoPage({
+    super.key,
+    required this.title,
+    required this.dados,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: Text(dados.length.toString()),
+      ),
+    );
+  }
+}
