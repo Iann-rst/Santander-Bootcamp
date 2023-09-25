b998f6e129ea511b6a11be86c608e52da47b3ebd
Iann Rodrigues <iannrst11@gmail.com>
2023-09-25 17:02:03 -0300

    feat: Conhecendo o Drawer

--- projetos_flutter/diologin/lib/pages/home_page.dart
+++ projetos_flutter/diologin/lib/pages/home_page.dart
@@ -10,9 +10,60 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Home Page"),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          title: const Text("Home Page"),
+        ),
+        drawer: Drawer(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                InkWell(
+                  child: Container(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 2),
+                    width: double.infinity,
+                    child: const Text("Dados cadastrais"),
+                  ),
+                  onTap: () {},
+                ),
+                const Divider(),
+                const SizedBox(
+                  height: 10,
+                ),
+                InkWell(
+                  child: Container(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 2),
+                    width: double.infinity,
+                    child: const Text("Configurações"),
+                  ),
+                  onTap: () {},
+                ),
+                const Divider(),
+                const SizedBox(
+                  height: 10,
+                ),
+                InkWell(
+                  child: Container(
+                    padding:
+                        const EdgeInsets.symmetric(vertical: 5, horizontal: 2),
+                    width: double.infinity,
+                    child: const Text("Termos de uso e privacidade"),
+                  ),
+                  onTap: () {},
+                ),
+                const Divider(),
+                const SizedBox(
+                  height: 10,
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
