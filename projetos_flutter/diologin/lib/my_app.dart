377f162d729b0faf24532243a8826ea79b83b210
Iann Rodrigues <iannrst11@gmail.com>
2023-09-29 14:16:47 -0300

    feat: add configuração de idioma no projeto

--- projetos_flutter/diologin/lib/my_app.dart
+++ projetos_flutter/diologin/lib/my_app.dart
@@ -1,5 +1,6 @@
 import 'package:diologin/pages/login_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MyApp extends StatelessWidget {
@@ -13,6 +14,12 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.red,
         textTheme: GoogleFonts.robotoTextTheme(),
       ),
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+      ],
+      supportedLocales: const [
+        Locale('pt'),
+      ],
       home: const LoginPage(),
     );
   }
