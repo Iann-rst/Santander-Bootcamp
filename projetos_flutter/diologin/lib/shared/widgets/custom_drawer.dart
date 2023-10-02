bb7b904d1c55470d74b15161421141ffcebb7149
Iann Rodrigues <iannrst11@gmail.com>
2023-10-02 12:03:56 -0300

    feat: Adicionando showModalBottomSheet - Modal

--- projetos_flutter/diologin/lib/shared/widgets/custom_drawer.dart
+++ projetos_flutter/diologin/lib/shared/widgets/custom_drawer.dart
@@ -10,14 +10,46 @@ class CustomDrawer extends StatelessWidget {
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          UserAccountsDrawerHeader(
-            currentAccountPicture: CircleAvatar(
-              backgroundColor: Colors.white,
-              child: Image.network(
-                  "https://hermes.digitalinnovation.one/assets/diome/logo.png"),
+          InkWell(
+            onTap: () {
+              showModalBottomSheet(
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(10),
+                      topRight: Radius.circular(10),
+                    ),
+                  ),
+                  context: context,
+                  builder: (BuildContext bc) {
+                    return Wrap(
+                      children: [
+                        ListTile(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          title: const Text("Camera"),
+                          leading: const Icon(Icons.camera),
+                        ),
+                        ListTile(
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                          title: const Text("Galeria"),
+                          leading: const Icon(Icons.photo),
+                        ),
+                      ],
+                    );
+                  });
+            },
+            child: UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundColor: Colors.white,
+                child: Image.network(
+                    "https://hermes.digitalinnovation.one/assets/diome/logo.png"),
+              ),
+              accountName: const Text("Iann Rodrigues"),
+              accountEmail: const Text("email@email.com"),
             ),
-            accountName: const Text("Iann Rodrigues"),
-            accountEmail: const Text("email@email.com"),
           ),
           InkWell(
             child: Container(
@@ -81,7 +113,37 @@ class CustomDrawer extends StatelessWidget {
                 ],
               ),
             ),
-            onTap: () {},
+            onTap: () {
+              showModalBottomSheet(
+                  context: context,
+                  builder: (BuildContext bc) {
+                    return Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 16, vertical: 12),
+                      child: const Column(
+                        children: [
+                          Text(
+                            "Termos de uso e privacidade",
+                            style: TextStyle(
+                              fontSize: 22,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(
+                            height: 16,
+                          ),
+                          Text(
+                            "A equipe de suporte precisa saber que um erro não identificado corrigiu o bug de estados estáticos nos componentes da UI.\n\nExplica pro Product Onwer que a normalização da data causou o bug na criação de novos polyfills para suportar os processos",
+                            textAlign: TextAlign.justify,
+                            style: TextStyle(
+                              fontSize: 16,
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  });
+            },
           ),
           const Divider(),
           const SizedBox(
